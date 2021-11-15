ea07d1c43e8bb1d84c06d17e457b2ee021a778d8
88Andybird <anbird88@gmail.com>
2021-11-15 17:27:19 +0000

    set up basic project files

---
+++ spec/spec_helper.rb
@@ -0,0 +1,44 @@
+require 'rspec'
+require 'capybara'
+require 'sinatra'
+#require 'sinatra-reloader' if development?
+require 'capybara/rspec'
+require 'pg'
+require 'simplecov'
+require 'simplecov-console'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+
+ENV['ENVIRONMENT'] = 'test'
+
+#Capybara.app = Makersbnb
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+
+RSpec.configure do |config|
+ 
+  config.expect_with :rspec do |expectations|
+   
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  
+  config.mock_with :rspec do |mocks|
+    
+    mocks.verify_partial_doubles = true
+  end
+
+ 
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+ 
+end
+
+
