f55ec4786c6473cd4bcbf9cb03019a1764f67c4f
Dylan James <djame011@gold.ac.uk>
2021-11-16 14:49:49 +0000

    Book feature spec editted

--- spec/features/booking_spec.rb
+++ spec/features/booking_spec.rb
@@ -1,8 +1,9 @@
 feature 'book a space' do
   scenario 'a user can book a space for at least 1 night' do
-    visit '/'
-    click_button 'Book Spaces'
-    click_button 'Book'
+    # visit '/'
+    # click_button 'Book Spaces'
+    # click_button 'Book'
+    visit('/book')
     expect(page).to have_content('Name:')
     expect(page).to have_button('Submit Request')
   end
