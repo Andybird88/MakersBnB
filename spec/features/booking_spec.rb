8bbc63a2677c1ae227e72df0bc188addbad954dc
Ginny <ginnynjon@gmail.com>
2021-11-16 14:31:45 +0000

    corrected book spec links

--- spec/features/booking_spec.rb
+++ spec/features/booking_spec.rb
@@ -1,9 +1,8 @@
 feature 'book a space' do
   scenario 'a user can book a space for at least 1 night' do
-    visit '/book'
-    # visit '/'
-    # click_button 'Book Spaces'
-    # click_button 'Book'
+    visit '/'
+    click_button 'Book Spaces'
+    click_button 'Book'
     expect(page).to have_content('Name:')
     expect(page).to have_button('Submit Request')
   end
