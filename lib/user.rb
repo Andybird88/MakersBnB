cbe201cbab15c92f87e56305a59aaf9d26212438
88Andybird <anbird88@gmail.com>
2021-11-17 16:07:28 +0000

    added user class.create method and test

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,22 @@
 class User
 
-    attr_reader :username
+    attr_reader :name, :username, :email, :password
 
-  def self.create
-    @username = "John_makers"
+    def initialize(name:, username:, email:, password:)
+      @name = name
+      @username = username
+      @email = email
+      @password = password
+    end
+
+  def self.create(name:, username:, email:, password:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
+    connection.exec("INSERT INTO users (u_name, username, email, password) VALUES ('#{name}','#{username}','#{email}','#{password}');")
+    
   end
 
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,9 +1,12 @@
 require_relative '../lib/user'
 
 describe User do
-  describe '#.create' do
+  describe '.#create' do
     it 'creates a new user' do
-      expect(User.create).to eq "John_makers"
+      test = User.create(name: 'John', username:'John_makers', email: 'test11@test.com', password:'test')
+      expect(test['email']).to_be "test11@test.com"
+ 
     end
   end
 end
+
