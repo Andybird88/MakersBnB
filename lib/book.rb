451eff0c7b0d993db59d8c3ddd277ccf0736c0d2
Dylan James <djame011@gold.ac.uk>
2021-11-18 12:10:38 +0000

    refactoring the book.create test

--- lib/book.rb
+++ lib/book.rb
@@ -2,11 +2,15 @@ require 'pg'
 
 class Book
 
-  attr_reader :start_date, :end_date
+  attr_reader :b_id, :start_date, :end_date, :approved # :user_id, :space_id
 
-  def initialize(start_date:, end_date:)
+  def initialize(b_id:, start_date:, end_date:, approved:)
+    @b_id = b_id
+    # @user_id = user_id
+    # @space_id = space_id
     @start_date = start_date
     @end_date = end_date
+    @approved = approved
   end
 
   def self.create(start_date:, end_date:)
@@ -15,8 +19,7 @@ class Book
     else
       connection = PG.connect(dbname: 'makersbnb')
     end
-
-    result = connection.exec("INSERT INTO bookings (start_date, end_date) VALUES ('#{start_date}', '#{end_date}') RETURNING start_date, end_date;")
-    Book.new(start_date: result[0]['start_date'], end_date: result[0]['end_date'])
+    result = connection.exec("INSERT INTO bookings (start_date, end_date) VALUES ('#{start_date}', '#{end_date}') RETURNING b_id, start_date, end_date;")
+    Book.new(b_id: result[0]['b_id'], start_date: result[0]['start_date'], end_date: result[0]['end_date'], approved: result[0]['approved'])
   end
 end
